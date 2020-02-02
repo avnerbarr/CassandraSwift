d8e6298430308c13b58dc63912f9eb093e8fd7b9
Avner Barr <avner.barr@gmail.com>
2020-02-02 17:15:07 +0200

    try again

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "cassandra-swift",
-            dependencies: []),
+            dependencies: ["CCassandra"]),
         .systemLibrary(
                     name: "CCassandra",
                     path: "Sources/cassandra",
