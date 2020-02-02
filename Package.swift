016ca143479e8b2fe5bf12aad34a188c9c01a20d
Avner Barr <avner.barr@gmail.com>
2020-02-02 17:24:59 +0200

    try

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "cassandra-swift",
-            targets: ["cassandra-swift"]),
+          name: "CCassandra",
+          targets: ["CCassandra"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,9 +18,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "cassandra-swift",
-            dependencies: ["CCassandra"]),
         .systemLibrary(
                     name: "CCassandra",
                     path: "Sources/cassandra",
